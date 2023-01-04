226e42e9d0aa7f15d3819bb22fc059a71be0d013
Tonkoshkur.Uliana <mikityukus@gmail.com>
2023-01-04 15:46:27 +0200

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Введіть число Фібоначі");
+        Scanner scan = new Scanner(System.in);
+        int fibonachi = scan.nextInt();
+
+        ExecutorService executable1 = Executors.newFixedThreadPool(5);
+        ExecutorService executable2 = Executors.newCachedThreadPool();
+        ExecutorService executable3 = Executors.newSingleThreadExecutor();
+
+
+        MyThread myThread = new MyThread(fibonachi, 1000);
+        myThread.run();
+        try {
+            myThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        RunnableThread myRunnable = new RunnableThread(fibonachi, 1000);
+
     }
 }
