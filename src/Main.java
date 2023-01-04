9426c0613ad043dd17d88a63e35be2cc7c96486b
Tonkoshkur.Uliana <mikityukus@gmail.com>
2023-01-04 15:48:20 +0200

    add

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,8 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int fibonachi = scan.nextInt();
 
-        ExecutorService executable1 = Executors.newFixedThreadPool(5);
-        ExecutorService executable2 = Executors.newCachedThreadPool();
-        ExecutorService executable3 = Executors.newSingleThreadExecutor();
-
-
         MyThread myThread = new MyThread(fibonachi, 1000);
         myThread.run();
-        try {
-            myThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         RunnableThread myRunnable = new RunnableThread(fibonachi, 1000);
 
     }
